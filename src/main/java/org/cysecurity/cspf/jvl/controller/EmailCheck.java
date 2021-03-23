f16eeb78172769980e5296e91da0d7f3c4a6dd38
Meera Rao <meerabs@yahoo.com>
2021-03-23 17:05:59 -0400

    Added a new email request method

--- src/main/java/org/cysecurity/cspf/jvl/controller/EmailCheck.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/EmailCheck.java
@@ -66,6 +66,38 @@ public class EmailCheck extends HttpServlet {
         }
     }
 
+   protected void processEmailRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        try {
+               Connection con=new DBConnect().connect(getServletContext().getRealPath("/WEB-INF/config.properties"));
+               String email=request.getParameter("email").trim();
+               JSONObject json=new JSONObject();
+                if(con!=null && !con.isClosed())
+                {
+                    ResultSet rs=null;
+                    Statement stmt = con.createStatement();  
+                    rs=stmt.executeQuery("select * from users where email='"+email+"'");
+                    if (rs.next()) 
+                    {  
+                     json.put("available", "1"); 
+                    }  
+                    else
+                    {  
+                      json.put("available", new Integer(0));  
+                    }  
+                }
+                out.print(json);
+        } 
+        catch(Exception e)
+        {
+            out.print(e);
+        }
+        finally {
+            out.close();
+        }
+    }
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -79,6 +111,7 @@ public class EmailCheck extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        processEmailRequest(request, response);
     }
 
     /**
@@ -93,6 +126,7 @@ public class EmailCheck extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        processEmailRequest(request, response);
     }
 
     /**
